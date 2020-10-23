57d6281957cfafe80d0358f6d50ccdc0d7a67d2e
konderev <konderev@raveinfosys.com>
2020-10-23 22:54:57 +0300

    remove section from layout to main

--- templates/layout.php
+++ templates/layout.php
@@ -216,9 +216,7 @@
     </div>
   </header>
 
-  <section class="page__main page__main--popular">
-    <?php print($content); ?>
-  </section>
+  <?php print($content); ?>
 
   <footer class="footer">
     <div class="footer__wrapper">
